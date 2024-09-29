bb9e420c7c22650c2296fc4cd08abb9392a49280
anilranamgr <akneel.rana@gmail.com>
2024-09-29 11:00:57 +1000

    Moved configs.yaml and made model size changes in example.py

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ def main():
 
   config = embodied.Config(dreamerv3.Agent.configs['defaults'])
   config = config.update({
-      **dreamerv3.Agent.configs['size100m'],
+      **dreamerv3.Agent.configs['size12m'], #changed from 100m to 12m , select simple available model 
       'logdir': f'~/logdir/{embodied.timestamp()}-example',
       'run.train_ratio': 32,
   })
